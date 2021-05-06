1aa00602f7f0a614a16e40ee5794932e307d813d
Andrea-Monges <83629379+Andrea-Monges@users.noreply.github.com>
2021-05-06 07:37:54 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let captcha ="ApE241";
+
+let captchaInDom= document.querySelector("#captcha");
+captchaInDom.innerHTML=captcha;
+
+
+let submit = document.querySelector("#btn-enviar");
+
+
+submit.addEventListener("click",function (e){
+
+    let respuesta = document.querySelector("#input-respuesta");
+    if (respuesta.value == captcha){
+        
+        prompt("el captcha es valido");
+    }else
+    {
+        prompt("el captcha es invalido");
+    }
+})
+
+
+    
