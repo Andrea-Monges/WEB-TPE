5755e62bcccf4a4f539d5dd5d77f8d855d97d017
Andrea-Monges <83629379+Andrea-Monges@users.noreply.github.com>
2021-06-03 17:38:13 -0300

    Add files via upload

--- tpe web responsive/js/galeria.js
+++ tpe web responsive/js/galeria.js
@@ -1,3 +1,4 @@
+"use strict";
 let index = 0;
 let imagenes = [];
 imagenes = document.querySelectorAll("img.oculto");
--- tpe web responsive/js/menuresponsive.js
+++ tpe web responsive/js/menuresponsive.js
@@ -1,5 +1,21 @@
+"use strict";
 document.querySelector(".btn-menu").addEventListener("click", toggleMenu);
 
 function toggleMenu() {
     document.querySelector(".navigation").classList.toggle("show");
 }
+
+//Ver más en: index.html//
+let i = 0;
+let button = document.querySelector(".btn-leermas").addEventListener("click", function () {
+    if (!i) {
+        document.getElementById("leermas").style.display = "inline";
+        document.querySelector(".btn-leermas").innerHTML = "Leer Menos";
+        i = 1;
+    }
+    else {
+        document.getElementById("leermas").style.display = "none";
+        document.querySelector(".btn-leermas").innerHTML = "Leer Más";
+        i = 0;
+    }
+})
